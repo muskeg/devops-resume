b8bb713bfd2c3c7d270064108bac9cdcfdc623e1
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-07-18 13:25:43 -0400

    changed website views.py

--- app/website/views.py
+++ app/website/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is the index page")
+    return HttpResponse("This is the index page")
