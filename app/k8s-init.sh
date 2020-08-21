a545d0ff62ede0d3b098bce6ed383599c1d02a1b
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-21 15:17:30 -0400

    init script for kubernetes

---
+++ app/k8s-init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+    echo "Looking for $SQL_HOST on port $SQL_PORT"
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python /home/app/web/manage.py migrate --noinput
+python /home/app/web/manage.py collectstatic --no-input
+cp -r ./raph/static/ ./static/
+/usr/local/bin/gunicorn raph.wsgi:application --bind 0.0.0.0:8000
+
+
