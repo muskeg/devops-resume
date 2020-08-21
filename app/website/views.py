8944d437784601db5d634eb2df16527ba46db462
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-21 10:34:41 -0400

    testing django app update deployment

--- app/website/views.py
+++ app/website/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hey David, this is the index page. It's fully deployed automatically to kubernetes through a Jenkins pipeline building and pushing docker images to a private registry, as defined in git repo, using an NGINX reverse proxy to show you that page.")
+    return HttpResponse("This is the index page. It's fully deployed automatically to kubernetes through a Jenkins pipeline building and pushing docker images to a private registry, as defined in git repo, using an NGINX reverse proxy to show you that page.")
