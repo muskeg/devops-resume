bb988d8196e29e760e631ac825278dd692d127c3
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-23 00:52:24 -0400

    in construction

--- app/website/views.py
+++ app/website/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     title = "muskegg.com"
-    description = "This is the index page. It's a Django app fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The app (including the page you're reading right now) is reachable through an NGINX reverse proxy also automatically deployed through the same pipeline."
+    description = "This is the index page of a Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through an NGINX reverse proxy also automatically deployed through the same pipeline."
     return render(request, 'web/index.html', {'title': title, 'description': description})
