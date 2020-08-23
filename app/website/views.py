03863f8f4ac0965e144a27e886132dd404b9ce5d
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-23 15:46:35 -0400

    footer

--- app/website/views.py
+++ app/website/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    footer_text = " "
     section_title = "this website"
     section_text = "This website is a Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through an NGINX reverse proxy also automatically deployed through the same pipeline."
-    return render(request, 'web/index.html', {'section_title': section_title, 'section_text': section_text})
+    return render(request, 'web/index.html', {'section_title': section_title, 'section_text': section_text, 'footer_text': footer_text})
