ef47973d4d69a3f9b4226389e566043a934c2a76
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-23 11:58:36 -0400

    linting python views code

--- app/website/views.py
+++ app/website/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    section-title = "this website"
+    section_title = "this website"
     description = "This website is a Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through an NGINX reverse proxy also automatically deployed through the same pipeline."
-    return render(request, 'web/index.html', {'title': section-title, 'description': description})
+    return render(request, 'web/index.html', {'section-title': section_title, 'description': description})
