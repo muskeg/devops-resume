e9719cd63c94e74a1641c8fba9e16dc9d6cef46b
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-22 21:46:45 -0400

    building django templates

--- app/website/views.py
+++ app/website/views.py
@@ -6,6 +6,4 @@ from django.http import HttpResponse
 def index(request):
     title = "muskegg.com"
     description = "This is the index page. It's a Django app fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The app (including the page you're reading right now) is reachable through an NGINX reverse proxy also automatically deployed through the same pipeline."
-    return render(request, 'web/base.html', {'title': title, 'description': description })
-
-
+    return render(request, 'web/base.html', {'title': title, 'description': description})
