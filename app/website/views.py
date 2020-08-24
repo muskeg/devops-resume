021f9faa612d815309b7c94f07c632e3fbfb7cb7
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-24 00:57:43 -0400

    styles, scripts and text

--- app/website/views.py
+++ app/website/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def index(request):
     footer_text = " "
     section_title = "this website"
-    section_text = """purpose of showing off some techniques and projects while actually adding tools to my skills set. My old portfolio was also over 10-year old and now largely irrelevant, it needed a refresh.
+    section_text = """As I got more interested in DevOps, I decided to setup this website that would serve the double purpose of showing off some techniques and projects while actually adding tools to my skills set. My old portfolio was also over 10-year old and now largely irrelevant, it needed a refresh.
 
 I ended with this project with plenty of the current buzzwords: a Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through a NGINX reverse proxy also automatically deployed through the same pipeline."""
     return render(request, 'web/index.html', {'section_title': section_title, 'section_text': section_text, 'footer_text': footer_text})
