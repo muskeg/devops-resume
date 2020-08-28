94be29a215fc600a56779540c212a8637f2e703a
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-08-28 10:27:36 -0400

    typos

--- app/website/views.py
+++ app/website/views.py
@@ -8,11 +8,11 @@ def index(request):
     section_pipeline_title = "this website"
     section_pipeline_intro = """As I got more interested in DevOps, I decided to setup this website that would serve the double purpose of showing off some techniques and projects while effectively adding new tools to my skills set. My old portfolio was also over 10-year old and now largely irrelevant, it needed a refresh.
 The idea:
-<span style="font-style: italic; font-weight: bold;">A Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public GIT repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through a NGINX reverse proxy also automatically deployed through the same pipeline</span>.
+<span style="font-style: italic; font-weight: bold;">A Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public Git repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through a NGINX reverse proxy also automatically deployed through the same pipeline</span>.
 These lines, full of buzzwords, were originally some &quot;hello world&quot; I used while developping the project. I keep them here as a reminder of the work I've done since the beginning.
 """
     section_pipeline_subtitle = "the pipeline"
-    section_pipeline_pipeline = "code, push, hook, build, test, tag, push, deploy, pull"
+    section_pipeline_pipeline = "code, commit, hook, build, test, tag, push, pull, deploy"
     content = {
         'section_pipeline_title': section_pipeline_title,
         'section_pipeline_intro': section_pipeline_intro,
