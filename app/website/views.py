6d11854de85e96ba55b8f4fcfdde72d4f8be89c7
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-09-02 18:02:55 -0400

    about me

--- app/website/views.py
+++ app/website/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     footer_text = " "
+    # Content for pipeline section
     section_pipeline_title = "this website"
     section_pipeline_intro = """As I got more interested in DevOps, I decided to setup this website that would serve the double purpose of showing off some techniques and projects while effectively adding new tools to my skills set. My old portfolio was also over 10-year old and now largely irrelevant, it needed a refresh.
     The idea:
@@ -24,6 +25,19 @@ def index(request):
     section_pipeline_deploy_title = "Deployment"
     section_pipeline_deploy = """Once the images are tagged and pushed, Jenkins tells my master Kubernetes node to update the Django app to the version it just built. The Kubernetes “cluster” is currently just 2 nodes running on Rancher’s k3s on servers I manage. Since the cluster is provisioned with Ansible, adding new nodes only takes a few minutes. Since the app is not running in a traditional cloud environment, I decided to use a self-provisioned public edge proxy that will forward all http traffic to the cluster.
     """
+    # Content for 'raph' section
+    section_raph_title = "About me"
+    section_raph_intro = """I have been an IT professional for the best part of my life. I used to specialize in data storage technologies. From data recovery to data protection. Doing some electronics work on failed hard disk drive? Building ZFS storage arrays? Archive 15 years of M&E projects on LTO tapes? Been there, done that. Nowadays, I see myself as a Linux and Windows Systems Administrator. With a passion for automation and scripting, slowly getting more involved in anything DevOps.  
+
+    I’m currently living in Montreal and I’m working as a senior systems administrator for Moment Factory. I began working for Moment Factory back in 2014 as sysadmin and tech support. Supporting the 3D and content creation pipeline and managing the storage and archiving systems. 
+
+    I left the company in 2017 to go play with the much larger infrastructure at Framestore, an international VFX Studio as a senior sysadmin. I quickly became the “Dr. Storage” in the Montreal team, managing large storage buckets in various availability tiers. With the large render farm and the numerous servers we would provision and manage, I discovered the joys of configuration management even though we were using an outdated version of Puppet back then. Even if I always enjoyed working with Linux, the environment there being (almost) 100% Linux-based gave me the chance to largely improve my skills. Framestore also having studios all over the world (London, New York, Chicago, Los Angeles and Pune) connected through VPLS, it was the perfect playground to play with larger-scale networking, and switches and firewalls management.
+
+    I heard the Moment Factory call once again in 2019 where I was hired in a more senior position where I could have a larger influence on projects and infrastructure’s development. I also had the opportunity to work on a huge client’s project as a DevOps support/consultant where we implemented an Ansible pipeline for the deployment of proprietary software and services. As everybody in IT knows, 2020 brought up great challenges for the development of networking and remote work. This gave me the opportunity to configure and tests many firewall and VPN products and get more experience with VMware’s software-defined networking solutions. 
+    """
+    section_raph_muskeg_title = "muskeg"
+    section_raph_muskeg = "The muskeg is where I grew up. It’s a type of terrain created by the accumulation of water and sediments on the near-surface bedrock preventing drainage. This type of terrain can be found all over Abitibi, my home region. I always loved the imagery of that rough terrain hard to navigate when you’re not used to it. "
+    # Content dictionnary
     content = {
         'section_pipeline_title': section_pipeline_title,
         'section_pipeline_intro': section_pipeline_intro,
@@ -35,6 +49,10 @@ def index(request):
         'section_pipeline_jenkins': section_pipeline_jenkins,
         'section_pipeline_deploy_title': section_pipeline_deploy_title,
         'section_pipeline_deploy': section_pipeline_deploy,
+        'section_raph_title': section_raph_title,
+        'section_raph_intro': section_raph_intro,
+        'section_raph_muskeg_title': section_raph_muskeg_title,
+        'section_raph_muskeg': section_raph_muskeg,
         'footer_text': footer_text
     }
     return render(request, 'web/index.html', content)
