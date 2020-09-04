fb874d5e26ab4e65590184231f0d0ec365ea41c0
Raphael Lanouette <raphaell@mf-mac-409.local>
2020-09-04 16:15:41 -0400

    missing alt descriptions to images

--- app/website/views.py
+++ app/website/views.py
@@ -9,7 +9,7 @@ def index(request):
     section_pipeline_title = "this website"
     section_pipeline_intro = """As I got more interested in DevOps, I decided to setup this website that would serve the double purpose of showing off some techniques and projects while effectively adding new tools to my skills set. My old portfolio was also over 10-year old and now largely irrelevant, it needed a refresh.
     The idea:
-    <div style="font-style: italic; font-size: smaller; width: 95%; margin: auto 2.5vh;">"A Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public Git repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through a NGINX reverse proxy also automatically deployed through the same pipeline."</div>
+    <span style="display: block; font-style: italic; font-size: smaller; width: 95%; margin: auto 2.5vh;">"A Django project fully deployed automatically to a Kubernetes bare-metal cluster through a Jenkins declarative pipeline that builds, tags and pushes Docker images to a private registry. Everything is defined in a public Git repo using git-secrets to hide sensitive information. The project (including this app page you're reading right now) is reachable through a NGINX reverse proxy also automatically deployed through the same pipeline."</span>
     These lines, full of buzzwords, were originally some &quot;hello world&quot; I used while developping the project. I keep them here as a reminder of the work I've done since the beginning.
     """
     section_pipeline_subtitle = "the pipeline"
